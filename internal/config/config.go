3221d7f7e31db28db3246e6506904f086eba2a42
Korrigon <korrig@goletan.tech>
2024-12-18 16:34:25 +0100

    - fix nil logger

--- internal/config/config.go
+++ internal/config/config.go
@@ -12,7 +12,7 @@ var cfg types.ObservabilityConfig
 // LoadObservabilityConfig loads the observability configuration settings from a predefined source.
 // It returns the loaded ObservabilityConfig struct and an error if the configuration loading fails.
 func LoadObservabilityConfig(log *logger.ZapLogger) (*types.ObservabilityConfig, error) {
-	if err := config.LoadConfig("Observability", &cfg, nil); err != nil {
+	if err := config.LoadConfig("Observability", &cfg, log); err != nil {
 		log.Error("Failed to load observability configuration", zap.Error(err))
 		return nil, err
 	}
